04c52dbc256a6d1c81f7f1928457bf91855986d2
neokaiyuan <kai@rocketacademy.co>
2023-11-27 13:18:44 +0800

    Reset profile name, description, image

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
-const name = "Neo Kai Yuan";
+const name = "My Name";
 export const siteTitle = "Next.js Sample Website";
 
 export default function Layout({ children, home }) {
